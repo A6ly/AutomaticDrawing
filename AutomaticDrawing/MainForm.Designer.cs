b4f1037517d8128fa2a73ab967a70ecde42a7b57
A6ly <a6ly26@gmail.com>
2020-02-18 11:14:42 +0900

    Commit

---
+++ AutomaticDrawing/MainForm.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace AutomaticDrawing
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.ImportImageButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ImportImageButton
+            // 
+            this.ImportImageButton.BackColor = System.Drawing.Color.White;
+            this.ImportImageButton.ForeColor = System.Drawing.Color.Black;
+            this.ImportImageButton.Location = new System.Drawing.Point(1, 2);
+            this.ImportImageButton.Name = "ImportImageButton";
+            this.ImportImageButton.Size = new System.Drawing.Size(106, 31);
+            this.ImportImageButton.TabIndex = 0;
+            this.ImportImageButton.Text = "Import Image";
+            this.ImportImageButton.UseVisualStyleBackColor = false;
+            this.ImportImageButton.Click += new System.EventHandler(this.ImportImageButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(322, 293);
+            this.Controls.Add(this.ImportImageButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Automatic Drawing";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ImportImageButton;
+    }
+}
+
---
+++ AutomaticDrawing/MainForm.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices; 
+using System.Windows.Forms;
+
+namespace AutomaticDrawing
+{
+    public partial class MainForm : Form
+    {
+        [DllImport("user32.dll")]
+        private static extern int RegisterHotKey(int hwnd, int id, int fsModifiers, int vk);
+
+        [DllImport("user32.dll")]
+        private static extern int UnregisterHotKey(int hwnd, int id);
+
+        [DllImport("user32.dll")]
+        internal static extern uint SendInput(uint nInputs, [MarshalAs(UnmanagedType.LPArray), In] INPUT[] pInputs, int cbSize);
+
+        [DllImport("user32.dll")]
+        private static extern bool SetCursorPos(int x, int y);
+
+        [Flags]
+        internal enum MOUSEEVENTF : uint
+        {
+            ABSOLUTE = 0x8000,
+            HWHEEL = 0x01000,
+            MOVE = 0x0001,
+            MOVE_NOCOALESCE = 0x2000,
+            LEFTDOWN = 0x0002,
+            LEFTUP = 0x0004,
+            RIGHTDOWN = 0x0008,
+            RIGHTUP = 0x0010,
+            MIDDLEDOWN = 0x0020,
+            MIDDLEUP = 0x0040,
+            VIRTUALDESK = 0x4000,
+            WHEEL = 0x0800,
+            XDOWN = 0x0080,
+            XUP = 0x0100
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct MOUSEINPUT
+        {
+            internal int dx;
+            internal int dy;
+            internal int mouseData;
+            internal MOUSEEVENTF dwFlags;
+            internal uint time;
+            internal UIntPtr dwExtraInfo;
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        internal struct InputUnion
+        {
+            [FieldOffset(0)]
+            internal MOUSEINPUT mi;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct INPUT
+        {
+            internal uint type;
+            internal InputUnion IU;
+            internal static int Size => Marshal.SizeOf(typeof(INPUT));
+        }
+
+        string filename;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            RegisterHotKey((int)this.Handle, 100, 0, (int)Keys.F5);
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            UnregisterHotKey((int)this.Handle, 0);
+        }
+
+        private string getFileName()
+        {
+            using (var ofd = new OpenFileDialog())
+            {
+                if(ofd.ShowDialog() == DialogResult.OK)
+                {
+                    return ofd.FileName;
+                }
+            }
+            return null;
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+
+            if (m.Msg == (int)0x312)
+            {
+                Draw();
+            }
+        }
+
+        public static Bitmap ConvertToBlackAndWhite(Bitmap original)
+        {
+            Bitmap newBitmap = new Bitmap(original.Width, original.Height);
+
+            Graphics graphics = Graphics.FromImage(newBitmap);
+
+            ColorMatrix colorMatrix = new ColorMatrix(
+                new[] {
+                    new float[] {0.7f, 0.7f, 0.7f, 0, 0},
+                    new float[] {0.7f, 0.7f, 0.7f, 0, 0},
+                    new float[] {1.8f, 1.8f, 1.8f, 0, 0},
+                    new float[] {0, 0, 0, 1, 0},
+                    new float[] {-1, -1, -1, 0, 1}
+            });
+
+            ImageAttributes attributes = new ImageAttributes();
+
+            attributes.SetColorMatrix(colorMatrix);
+            attributes.SetThreshold(1);
+
+            graphics.DrawImage(original, new Rectangle(0, 0, original.Width, original.Height), 0, 0, original.Width, original.Height, GraphicsUnit.Pixel, attributes);
+
+            graphics.Dispose();
+
+            return newBitmap;
+        }
+
+        private void ImportImageButton_Click(object sender, EventArgs e)
+        {
+            filename = getFileName();
+
+            if(filename == null)
+            {
+                return;
+            }
+
+            Bitmap image = ConvertToBlackAndWhite((Bitmap)Image.FromFile(filename));
+            Size = new Size(image.Width, image.Height);
+
+            using (Graphics graphics = CreateGraphics())
+            {
+                graphics.Clear(Color.CornflowerBlue);
+                graphics.DrawImage(image, 0, 0, Width, Height);
+            }
+            MessageBox.Show("Start button is F5", "AutomaticDrawing", MessageBoxButtons.OK, MessageBoxIcon.None, MessageBoxDefaultButton.Button2, MessageBoxOptions.ServiceNotification);
+        }
+
+        void Draw()
+        {
+            Bitmap image = ConvertToBlackAndWhite((Bitmap)Image.FromFile(filename));
+
+            Point position = Cursor.Position;
+
+            ClickMouse(MouseButtons.Left, position.X, position.Y, true);
+            ClickMouse(MouseButtons.Left, position.X, position.Y, false);
+
+            for(int y = 0; y < image.Height; y++)
+            {
+                for (int x = 0; x < image.Width; x++)
+                {
+                    Color color = image.GetPixel(x, y);
+
+                    if(color.B == 0)
+                    {
+                        int drawX = position.X + x;
+                        int drawY = position.Y + y;
+                        SetCursorPos(drawX, drawY);
+                        ClickMouse(MouseButtons.Left, drawX, drawY, true);
+                        ClickMouse(MouseButtons.Left, drawX, drawY, false);
+                        Thread.Sleep(3);
+                    }
+                }
+            }
+            MessageBox.Show("I drew it all^^7", "AutomaticDrawing", MessageBoxButtons.OK, MessageBoxIcon.None, MessageBoxDefaultButton.Button2, MessageBoxOptions.DefaultDesktopOnly);
+        }
+
+        public static void ClickMouse(MouseButtons mouseButtons, int x, int y, bool nextLine)
+        {
+            var inputs = new INPUT[1];
+            var input = new INPUT { type = 0 };
+
+            input.IU.mi.dx = x;
+            input.IU.mi.dy = y;
+
+            switch(mouseButtons)
+            {
+                case MouseButtons.Left:
+                    input.IU.mi.dwFlags = nextLine ? MOUSEEVENTF.LEFTDOWN : MOUSEEVENTF.LEFTUP;
+                    break;
+                case MouseButtons.Right:
+                    input.IU.mi.dwFlags = nextLine ? MOUSEEVENTF.RIGHTDOWN : MOUSEEVENTF.RIGHTUP;
+                    break;
+                case MouseButtons.Middle:
+                    input.IU.mi.dwFlags = nextLine ? MOUSEEVENTF.MIDDLEDOWN : MOUSEEVENTF.MIDDLEUP;
+                    break;
+                case MouseButtons.XButton1:
+                case MouseButtons.XButton2:
+                    input.IU.mi.mouseData = (mouseButtons == MouseButtons.XButton1) ? 0x0001 : 0x0002;
+                    input.IU.mi.dwFlags = nextLine ? MOUSEEVENTF.XDOWN : MOUSEEVENTF.XUP;
+                    break;
+            }
+
+            inputs[0] = input;
+
+            SendInput(1, inputs, INPUT.Size);
+        }
+    }
+}
